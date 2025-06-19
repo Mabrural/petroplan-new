a763f13d10111ddc0a8f78a00ffb44cb362498f2
mabrural <mabruralmutaqi@gmail.com>
2025-06-19 20:38:34 +0700

    penghapusan route /register pada routes/auth.php, dan alihkan route / ke/login

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified', 'active'])->name('dashboard');
